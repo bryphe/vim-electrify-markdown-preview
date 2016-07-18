19a6237fff61c4a7063a7110b25cd519683c54ac
extr0py <extr0py@extropygames.com>
2016-07-18 08:16:25 -0700

    Initial markdown implementation

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # vim-electrify-markdown-preview
 ###### Live preview of markdown files
 
+- Hello World 3
+- What up yo yo yo
+
+- Hello World2
+
+``` javascript
+        window.location.reload();
+        var win = derp();
+```
+
 ## Example
 
+``` mermaid
+    graph TD;
+    aj
+    agja
+```
+
 ## Motivation
+
 
 ## Installation
 
 ## License
 
 MIT License
+
--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-vim.echo("Hello world!");
+const {BrowserWindow} = require("electron").remote;
+
+var currentWindow = null;
+// This is a more advanced scenario - this uses an IPC channel
+// to send data from the plugin -> BrowserWindow
+vim.addCommand("MarkdownPreview", function(context) {
+    if(currentWindow)
+        return;
+
+    currentWindow = new BrowserWindow({width: 800, height: 600, skipTaskbar: true, title: "vim-electrify-markdown-preview", autoHideMenuBar: true});
+    currentWindow.loadURL(`file://${__dirname}/markdown_preview.html`);
+
+    currentWindow.on("close", () => {
+        currentWindow = null;
+    });
+});
+
+vim.addCommand("MarkdownPreviewClose", (context) => {
+    if(!currentWindow)
+        return;
+
+    currentWindow.destroy();
+    currentWindow = null;
+});
+
+vim.on("BufferChanged", (args) => {
+
+    if(!currentWindow)
+        return;
+
+    currentWindow.webContents.send("content-update", args.newContents);
+});
